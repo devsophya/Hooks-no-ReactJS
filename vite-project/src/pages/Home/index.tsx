20bc81fb74925fb849209f118eb441796fe74597
Sophya <sophyadamiazodev@gmail.com>
2025-05-22 16:27:23 -0300

    feat: Enhance Home component with styled input fields and a custom button; improve user experience with focused styles and disabled state.

--- vite-project/src/pages/Home/index.tsx
+++ vite-project/src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
+  StartCountdownButton,
+  TaskInput,
 } from "./styles";
 
 export function Home() {
@@ -12,10 +15,10 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" />
 
           <span>minutos.</span>
         </FormContainer>
@@ -28,10 +31,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton disabled type="submit">
           <PlayCircle size={24} />
           Iniciar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   );
