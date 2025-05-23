c5f6e5bd47e57a49453cb23c4d0c05c3065e434c
Sophya <sophyadamiazodev@gmail.com>
2025-05-23 15:49:41 -0300

    feat: Integrate react-hook-form for improved form handling and validation in Home component.

--- vite-project/src/pages/Home/index.tsx
+++ vite-project/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { PlayCircle } from "@phosphor-icons/react";
+import { useForm } from "react-hook-form";
 import {
   CountdownContainer,
   FormContainer,
@@ -8,22 +9,27 @@ import {
   StartCountdownButton,
   TaskInput,
 } from "./styles";
-import { useState } from "react";
 
 export function Home() {
-  function handleSubmit(event) {
-    
+  const { register, handleSubmit, watch } = useForm();
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
   }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit} action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -40,6 +46,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -53,7 +60,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={!task} type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <PlayCircle size={24} />
           Iniciar
         </StartCountdownButton>
